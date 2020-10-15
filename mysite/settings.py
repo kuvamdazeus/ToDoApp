709736dd52f2a16042649ade5b97f261309715a4
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2020-10-15 17:52:25 +0530

    -_-

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*zmxn!bi5hamu0%fmgz_kb1ed21gw_b5wvy=n=202ou=)8g*!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'https://todoapp0529.herokuapp.com/']
 
